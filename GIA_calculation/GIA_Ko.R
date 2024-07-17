59cd3defe86f71e63bdf4f219869688f629fe030
songweii <80997213+songweii@users.noreply.github.com>
2024-07-17 16:27:11 +0800

    Add files via upload

---
+++ GIA_calculation/GIA_Ko.R
@@ -0,0 +1,14 @@
+load("CFA_Ko.RData")
+library(bruceR)
+library(lavaan)
+
+all_MLLMs_data <- import("MLLMs_score/MLLMs_acc_ko_score.xlsx")
+names(all_MLLMs_data)
+MLLMs_data <- all_MLLMs_data[,2:19]
+MLLMs_data %>% na.omit() %>% apply(2,scale) %>% as.data.frame() -> z_score_data  # normalize by column, zscore
+
+GIA_scores <- lavPredict(fit_behav2, newdata = MLLMs_data)
+GIA_scores[,6]
+
+cor.test(all_MLLMs_data$total_acc, GIA_scores[,6])
+GLM_summary(lm(all_MLLMs_data$total_acc~GIA_scores[,6]))
